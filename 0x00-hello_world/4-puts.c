e67681b7975269c55e8790df2c972767657a6bfb
Kirito-YO <ussf.lord@gmail.Com>
2023-06-15 13:10:28 -0700

    my first C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  *this is my first C code 
  *now i learn the function puts it was more intresstiong than printf
  */
-
 int main(void)
 {
+/* use puts not printf */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
