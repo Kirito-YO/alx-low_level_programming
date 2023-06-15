536ac72e852b5986ef50e67074e2a97a12d3680f
Kirito-YO <ussf.lord@gmail.Com>
2023-06-15 14:51:38 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main()
 {
-	size_of_quote=sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", size_of_quote);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
