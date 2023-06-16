327b89268c943216aac066d2447d1f3d4b575213
Kirito-YO <ussf.lord@gmail.Com>
2023-06-16 16:12:13 -0700

    zyxwvutsrqponmlkjihgfedcba

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ ** main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char *c = "zyxwvutsrqponmlkjihgfedcba";
+
+	do {
+		putchar(c[i]);
+		i++;
+	} while (c[i] != 0);
+	putchar('\n');
+return (0);
+}
