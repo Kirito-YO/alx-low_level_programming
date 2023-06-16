6de581e9151556cf9d1de78d35fcc2df3a5893c6
Kirito-YO <ussf.lord@gmail.Com>
2023-06-16 16:08:52 -0700

    putchar ++

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ ** main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	do {
+		putchar(i);
+		i++;
+	} while (i < 58);
+	putchar('\n');
+return (0);
+}
