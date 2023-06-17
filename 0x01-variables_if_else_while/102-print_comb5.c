519551dad899ad27bca4cc92d56ca8800c9049bc
Kirito-YO <ussf.lord@gmail.Com>
2023-06-17 14:04:26 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 				putchar(32);
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
-				if (i != 98 && j != 99)
+				if (i < 98 && j < 99)
 				{
 					putchar(44);
 					putchar(32);
