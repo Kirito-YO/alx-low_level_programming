0a5c74ea8d0fe22d618f8747fbe293805f4af815
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 08:01:03 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "main.h"
+/**
+ * print_alphabet - printa a..z.
+ *
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
-        int i = 97;
+	int i = 97;
 
-        do {
-                _putchar(i);
-                i++;
-        } while (i < 123);
-        _putchar('\n');
+	do {
+		_putchar(i);
+		i++;
+	} while (i < 123);
+	_putchar('\n');
 }
