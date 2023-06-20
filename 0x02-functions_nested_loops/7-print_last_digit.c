158b88f6529584f2238b17161d25ae833cc965e6
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 13:15:23 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,18 @@ int print_last_digit(int i)
 	{
 		i = i * -1;
 		i = i % 10;
+		_putchar('0' + i);
+		return (i);
 	}
 	else if (i > 9)
 	{
 		i = i % 10;
+		_putchar('0' + i);
+		return (i);
 	}
-	_putchar('0' + i);
-	return (i);
+	else
+	{
+		_putchar('0' + i);
+		return (i);
+	}
 }
