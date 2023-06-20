b25d89a3e395aa1a6f88e167bca748ae5caa8580
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 08:49:35 -0700

    islower?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _islower - printa a..z.
+ *
+ * Return: Always 0.
+ *
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+	
+}
