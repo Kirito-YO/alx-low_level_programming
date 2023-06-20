cf371853372a76e6433c2d28416126e9f28b0e07
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 07:20:46 -0700

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 void NewPutchar(char string[]);
+void print_alphabet(void);
+int _putchar(char c);
 void NewPutchar(char string[])
 {
 	printf("%s", string);
+}
+
+void print_alphabet(void)
+{
+	int i = 97;
+	
+	do {
+		_putchar(i);
+		i++;
+	} while (i < 123);
+	_putchar('\n');
 }
