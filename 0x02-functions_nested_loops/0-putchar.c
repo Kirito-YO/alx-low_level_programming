c3551db0e4c86ad9daf6a5537c588710d8cd954a
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 03:39:11 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  ** main - Entry point
  *
@@ -7,5 +7,5 @@
 
 int main(void)
 {
-	printf("\_putchar\n");
+	new_putchar("\_putchar\n");
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+char new_putchar(char string[])
+void new_putchar(char string[])
+{
+	printf("%s", string)
+	return (0);
+}
