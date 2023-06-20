5c5f0c258f2d7e42b15596b8defa29d4be354d1d
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 12:05:19 -0700

    sign of

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - is alpha ?
+ *
+ * Return: Always 0.
+ *
+ * @c: The character to print
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n = 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1)
+	}
+}
