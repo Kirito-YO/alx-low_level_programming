3e1fe72ced359ca91f6765468040455653f59fd1
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 15:20:58 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - times table.
+ *
+ * Return: Always 0.
+ *
+ */
+void times_table(void)
+{
+	int i, ii, s;
+	for (i = 0; i < 10; i++)
+	{
+		for (ii = 0; ii < 10; ii++)
+		{
+			s = i + ii;
+			_putchar(s + '0');
+			if (ii < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (s < 9)
+				{
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}	
