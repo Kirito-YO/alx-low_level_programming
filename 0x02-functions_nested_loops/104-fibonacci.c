228d97d2fedcee3e7e3dbe19c08a946c85df4337
Kirito-YO <ussf.lord@gmail.Com>
2023-06-21 15:49:47 -0700

    98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ ** main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long i, f1 = 0, f2 = 1, s;
+
+	for (i = 0; i < 98; i++)
+	{
+		s = f1 + f2;
+		printf("%lu", s);
+		f1 = f2;
+		f2 = s;
+		if (i == 97)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
