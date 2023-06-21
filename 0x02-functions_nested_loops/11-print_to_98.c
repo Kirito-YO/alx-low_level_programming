6cacebeb1829e7c4ffd70dbb4cdf26ebf1f71b8b
Kirito-YO <ussf.lord@gmail.Com>
2023-06-21 13:14:16 -0700

    98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,41 +7,31 @@
  */
 void print_to_98(int n)
 {
-	if (n < 0)
-	{
-		while (n <= 98)
-		{
+	if (n < 0) {
+		while (n <= 98) {
 			printf("%d", n);
-			if (n < 98)
-			{
+			if (n < 98) {
 				printf(", ");
 			}
 			n++;
 		}
 	}
-	else if (n >= 0 && n < 98)
-	{
-		while (n <= 98)
-		{
+	else if (n >= 0 && n < 98) {
+		while (n <= 98) {
 			printf("%d", n);
-			if (n < 98)
-			{
+			if (n < 98) {
 				printf(", ");
 			}
 			n++;
 		}
 	}
-	else if (n == 98)
-	{
+	else if (n == 98) {
 		printf("%d", n);
 	}
-	else
-	{
-		while (n >= 98)
-		{
+	else {
+		while (n >= 98) {
 			printf("%d", n);
-			if (n > 98)
-			{
+			if (n > 98) {
 				printf(", ");
 			}
 			n--;
