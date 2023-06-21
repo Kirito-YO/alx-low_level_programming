384b418452446bd95716afcd15c5ff246d791a32
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 17:30:24 -0700

    while 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,47 @@
  */
 void print_to_98(int n)
 {
+	int m, l;
+
 	do {
-		if (n < 98)
+		if (n < 9 && n < 98)
+		{
+			m = n / 10; 
+			l = n % 10;
+			_putchar(m);
+			_putchar(l);
+			n++;
+		}
+		else if (n => 0 && n < 10)
 		{
 			_putchar(n);
 			n++;
 		}
-		else
+		else if (n < 0 && n > -10)
 		{
 			_putchar(n);
+			n++;
+		}
+		else if (n > 98)
+		{
+			m = n / 10;
+			l = n % 10;
+			_putchar(m);
+			_putchar(l);
 			n--;
+		}
+		else
+		{
+			m = n / 10;
+			l = n % 10;
+			_putchar(m);
+			_putchar(l);
+			n++;
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
 		}
 	} while (n != 98);
 }
