d04f5f5eb8b285cf5e010b711352644243f9bbf6
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 17:13:57 -0700

    while 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_to_98 - add two int
+ *
+ * Return: Always 0.
+ *
+ */
+void print_to_98(int n)
+{
+	do {
+		if (n < 98)
+		{
+			_putchar(n);
+			n++;
+		}
+		else
+		{
+			_putchar(n);
+			n--;
+		}
+	} while (n != 98)
+}
