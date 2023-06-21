81a836932ae9df006e1471567bc8dcb3c7b08e5d
Kirito-YO <ussf.lord@gmail.Com>
2023-06-20 17:03:30 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add -add two int
+ *
+ * Return: Always 1.
+ *
+ * @a: The character to print
+ * @b: the number b
+ *
+ */
+int add(int a, int b)
+{
+	s = a + b;
+	return (s);
+}
