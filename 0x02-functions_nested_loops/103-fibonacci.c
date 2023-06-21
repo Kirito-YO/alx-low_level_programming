110c426607b13a57034ab3707461e5cdde9bd36b
Kirito-YO <ussf.lord@gmail.Com>
2023-06-21 15:43:04 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (i = 0; i < 50; i++)
 	{
-		if (f1 < 4000000 && (j % 2) == 0)
+		if (f1 < 4000000 && (f1 % 2) == 0)
 			n = n + f1;
 		s = f1 + f2;
 		f1 = f2;
 		f2 = s;
 	}
-	printf("%lu\n" n);
+	printf("%lu\n", n);
 	return (0);
 }
