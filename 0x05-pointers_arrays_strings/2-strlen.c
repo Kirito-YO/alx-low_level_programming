fd49b9a7f14b48c2b25b98502e7d279084fb7c08
Kirito-YO <ussf.lord@gmail.Com>
2023-06-26 12:14:02 -0700

    length of

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - a function.
+ *
+ * Return : always 0.
+ * @s: parameter
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	do {
+		i++;
+		s++;
+	} while (*s != 0);
+}
