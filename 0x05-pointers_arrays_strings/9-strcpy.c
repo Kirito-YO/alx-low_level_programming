8c27cf7c7fa98d31536eb496b29505c99083b3f2
Kirito-YO <ussf.lord@gmail.Com>
2023-06-26 14:23:21 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy - some.
+ *
+ * Return : char.
+ * @dest: parameter
+ * @src: parameter
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0; j = 0;
+
+	while (*(src + i) != 0)
+	{
+		i++;
+	}
+	for ( ; j < l ; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[i] = 0;
+	return (dest);
+}
