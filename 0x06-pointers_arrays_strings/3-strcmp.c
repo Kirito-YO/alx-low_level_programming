06ee4e3a0385cd5372755611065cfbf1c944fa87
Kirito-YO <ussf.lord@gmail.Com>
2023-06-29 05:20:33 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmpmy function.
+ *
+ * Return: return.
+ * @s1: var1.
+ * @s2: var2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != 0 && s2[i] != 0)
+	{
+		if (s1[i] != s2[i])
+				return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
