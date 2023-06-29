b3ca5c0bb94f70424c288e30444dc63cabc4df24
Kirito-YO <ussf.lord@gmail.Com>
2023-06-29 05:05:28 -0700

    2strcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings.
+ *
+ * Return: concatenation of two strings.
+ * @dest: first string.
+ * @src: seconde string.
+ * @n: int.
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+	while (src[j] != 0 && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
