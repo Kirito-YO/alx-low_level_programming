02ab86d97f1888abcf7fbc5b0a0fc2475998963c
Kirito-YO <ussf.lord@gmail.Com>
2023-07-03 12:42:47 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,30 +1,23 @@
 #include "main.h"
 /**
- * _strspn - function do this.
+ * _strpbrk - function do this.
  * @s: var.
  * @accept: var.
  *
  * Return: something.
  */
-
-unsigned int _strspn(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	unsigned int j = 0;
+	int i;
 
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
-			{
-				j++;
-				break;
-			}
-			else if (accept[i + 1] == 0)
-				return (j);
+				return (s);
 		}
 		s++;
 	}
-	return (j);
+	return (0);
 }
