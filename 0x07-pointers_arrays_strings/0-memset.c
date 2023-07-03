a6da2cf0a626817e3e2347cdf8c29ac9c4e63b34
Kirito-YO <ussf.lord@gmail.Com>
2023-07-03 10:03:15 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,15 +5,18 @@
  * @b: var.
  * @n: var.
  *
- * Return: Nothing.
+ * Return: s.
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	do {
+	while (n > 0)
+	{
 		s[i] = b;
 		n--;
 		i++;
-	} while (n > 0)
+	}
+	return (s);
 }
