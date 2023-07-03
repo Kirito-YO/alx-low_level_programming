137930cb89db1e476dc2aaa02f690611761953cd
Kirito-YO <ussf.lord@gmail.Com>
2023-07-03 13:40:12 -0700

    diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strstr - function do this.
+ * @a: var.
+ * @size: var.
+ *
+ * Return: something.
+ */
+void print_diagsums(int *a, int size);
+{
+	int i = 0, j = 0, n;
+
+	for (n = 0; n < size; n++)
+	{
+		i = i + a[n * size + n];
+	}
+	for (n = size - 1; n >= 0; n++)
+	{
+		j = j + a[y * size + (size - n - 1)];
+	}
+	printf("%d, %d\n", i, j);
+}
