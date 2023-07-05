748d47ae3aa26ecd7f877f3ecf17c2cedc40656c
Kirito-YO <ussf.lord@gmail.Com>
2023-07-05 07:15:18 -0700

    puts recrusion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(s[i]);
-	if (s[i] != 0)
-		_puts_recursion(s);
+	int i;
+	
+	for (i = 0; s[i] != 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
