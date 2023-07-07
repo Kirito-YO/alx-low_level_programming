fac2b1af58520010abc754b58304fb9806de2cf9
Kirito-YO <ussf.lord@gmail.Com>
2023-07-07 13:08:01 -0700

    prints all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function that do .
+ * Return: always will be 0.
+ * @argc: var.
+ * @argv: var.
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	  printf("%s\n", argv[i]);
+	}
+	return (0);
+}
