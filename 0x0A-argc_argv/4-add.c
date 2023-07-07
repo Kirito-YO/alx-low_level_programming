85aa4205df2bd581c61053984935fabf9396037d
Kirito-YO <ussf.lord@gmail.Com>
2023-07-07 14:28:21 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - function that do .
+ * Return: always will be 0.
+ * @argc: var.
+ * @argv: var.
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int a, r = 0, i;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			a = atoi(argv[i]);
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				r = r + a;
+			}
+		}
+		 printf("%d\n", r);
+	}
+	return (0);
+}
