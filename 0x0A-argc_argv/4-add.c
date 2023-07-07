7f62643e9a87817b6ea942a7b6752cac6d4cf5e5
Kirito-YO <ussf.lord@gmail.Com>
2023-07-07 15:53:03 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,15 +8,16 @@
  * @argv: var.
  *
  */
-int main(int argc, char *argv[])
+int main( int argc, char *argv[])
 {
-	unsigned int r = 0, i, a;
+	unsigned int r = 0, i, a, b;
 
 	if (argc == 1)
 		printf("0\n");
 	else
 	{
-		for (i = 1; i < argc; i++)
+		b = (unsigned int)(argc);
+		for (i = 1; i < b; i++)
 		{
 			a = atoi(argv[i]);
 			if (!isdigit(*argv[i]))
