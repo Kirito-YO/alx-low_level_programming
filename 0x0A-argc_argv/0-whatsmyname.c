f8a6298a7d8f2a0600b0bbd14ac48ed0e0ac2f7f
Kirito-YO <ussf.lord@gmail.Com>
2023-07-07 12:46:12 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - function that do .
- *
+ * Return: always will be 0.
  * @argc: var.
  * @argv: var.
- * Return : always 0.
+ *
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
