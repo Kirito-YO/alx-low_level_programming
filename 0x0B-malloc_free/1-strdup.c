22b590d5c3ff44a69e8c67095c54d1f01d08c714
Kirito-YO <ussf.lord@gmail.Com>
2023-07-10 09:55:37 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strdup - function can do that.
+ * @str: var1.
+ *
+ * Return: Nothing.
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int s = 0, i = 0;
+
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	while (str[s] != 0)
+	{
+		s++;
+	}
+	s++;
+	a = (char *) malloc(s *sizeof(char));
+	if (a == 0)
+	{
+		return (NULL);
+	}
+	while (str[i] != 0)
+	{
+		a[i] = str[i];
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
+}
