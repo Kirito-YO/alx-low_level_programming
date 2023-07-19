5161f7ea19f1f582043c69643bb7f4b28667e7ee
Kirito-YO <ussf.lord@gmail.Com>
2023-07-19 15:54:16 -0700

    task ~

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * main - main function check 
+ * main - main function check
  * @argc: var
  * @argv: var
  *
@@ -27,12 +27,12 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
